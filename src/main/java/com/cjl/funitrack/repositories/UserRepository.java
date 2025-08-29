1aae3d08bef4b6a59d7792c2edafb7586778c9ed
stefanybeacides <stefanybeacides@hotmail.com>
2025-08-29 16:50:19 -0300

    subida WebConfig

--- src/main/java/com/cjl/funitrack/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cjl.funitrack.repositories;
-
-import com.cjl.funitrack.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
